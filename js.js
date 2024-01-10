3c596317cad647885a789b732090e4ba6e2a8b6b
Onischenko Kostyantyn <Onischenko411@gmail.com>
2024-01-10 19:36:36 +0200

    file add

---
+++ js.js
@@ -0,0 +1,13 @@
+const num1 = +prompt('Введіть перше число')
+const num2 = +prompt('Введіть друге число')
+const resultDiv = num1 - num2
+const resultDif = num1 / num2
+const resultMult = num1 * num2
+const resultAdd = num1 + num2
+alert(`
+${num1} - ${num2} = ${resultDiv}
+${num1} + ${num2} = ${resultAdd}
+${num1} / ${num2} = ${resultDif}
+${num1} * ${num2} = ${resultMult}
+`)
+
